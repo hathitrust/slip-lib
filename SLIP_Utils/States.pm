6e37818e479c77610db13592b9c23a36b94587f3
Phillip Farber <pfarber@umich.edu>
2013-03-04 10:55:31 -0500

    * SLIP_Utils/States.pm: our

--- SLIP_Utils/States.pm
+++ SLIP_Utils/States.pm
@@ -26,55 +26,55 @@ use States;
 =cut
 
 # Queue states
-$Q_AVAILABLE  = 2;
-$Q_PROCESSING = 3;
+our $Q_AVAILABLE  = 2;
+our $Q_PROCESSING = 3;
 
 # Return codes check these
-$RC_OK                 = 0;
-$RC_DATABASE_CONNECT   = 1;
-$RC_MAX_ERRORS         = 2;
-$RC_CRITICAL_ERROR     = 3;
-$RC_BAD_ARGS           = 4;
-$RC_SOLR_ERROR         = 5;
-$RC_DRIVER_DISABLED    = 6;
-$RC_DRIVER_NO_SEM      = 7;
-$RC_DRIVER_WRONG_STAGE = 8;
-$RC_CHILD_ERROR        = 9;
-$RC_ERROR_SHARD_STATES = 10;
-$RC_NO_INDEX_DIR       = 11;
-$RC_DRIVER_BUSY_FILE   = 12;
-$RC_DRIVER_FLAGS_DIR   = 13;
-$RC_RIGHTS_NO_SEM      = 14;
-$RC_BAD_SCHED_FILE     = 15;
-$RC_TOMCAT_STOP_FAIL   = 16;
-$RC_TOMCAT_START_FAIL  = 17;
-$RC_WRONG_NUM_SEGS     = 18;
+our $RC_OK                 = 0;
+our $RC_DATABASE_CONNECT   = 1;
+our $RC_MAX_ERRORS         = 2;
+our $RC_CRITICAL_ERROR     = 3;
+our $RC_BAD_ARGS           = 4;
+our $RC_SOLR_ERROR         = 5;
+our $RC_DRIVER_DISABLED    = 6;
+our $RC_DRIVER_NO_SEM      = 7;
+our $RC_DRIVER_WRONG_STAGE = 8;
+our $RC_CHILD_ERROR        = 9;
+our $RC_ERROR_SHARD_STATES = 10;
+our $RC_NO_INDEX_DIR       = 11;
+our $RC_DRIVER_BUSY_FILE   = 12;
+our $RC_DRIVER_FLAGS_DIR   = 13;
+our $RC_RIGHTS_NO_SEM      = 14;
+our $RC_BAD_SCHED_FILE     = 15;
+our $RC_TOMCAT_STOP_FAIL   = 16;
+our $RC_TOMCAT_START_FAIL  = 17;
+our $RC_WRONG_NUM_SEGS     = 18;
 
 # Driver stages check these
-$St_Undefined       = 'Undefined';
-$St_Build_Wait      = 'Build_Wait';
-$St_Building        = 'Building';
-$St_Optimizing      = 'Optimizing';
-$St_Checking        = 'Checking';
-$St_Driver_ERROR    = 'Driver_ERROR';
+our $St_Undefined       = 'Undefined';
+our $St_Build_Wait      = 'Build_Wait';
+our $St_Building        = 'Building';
+our $St_Optimizing      = 'Optimizing';
+our $St_Checking        = 'Checking';
+our $St_Driver_ERROR    = 'Driver_ERROR';
 
 # Shard states
-$Sht_No_Build_Error = 0;
-$Sht_Build_Error    = 2;
+our $Sht_No_Build_Error = 0;
+our $Sht_Build_Error    = 2;
 
-$Sht_Not_Optimized  = 0;
-$Sht_Optimized      = 1;
-$Sht_Optimize_Error = 2;
+our $Sht_Not_Optimized  = 0;
+our $Sht_Optimized      = 1;
+our $Sht_Optimize_Error = 2;
 
-$Sht_Not_Checked    = 0;
-$Sht_Checked        = 1;
-$Sht_Check_Error    = 2;
+our $Sht_Not_Checked    = 0;
+our $Sht_Checked        = 1;
+our $Sht_Check_Error    = 2;
 
 # Snapshot release states
-$Srl_Rel_Error      = 0;
-$Srl_Rel_Ok         = 1;
-$Srl_Rel_Pending    = 2;
-$Srl_Rel_Wrong_Host = 3;
+our $Srl_Rel_Error      = 0;
+our $Srl_Rel_Ok         = 1;
+our $Srl_Rel_Pending    = 2;
+our $Srl_Rel_Wrong_Host = 3;
 
 # ---------------------------------------------------------------------
 1;
